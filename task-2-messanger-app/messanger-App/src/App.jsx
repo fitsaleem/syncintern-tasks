fb58020f16338a547c62936d1ce43a6fe2bd4329
fitsaleem <sherohero33@gmail.com>
2023-04-25 02:07:25 +0500

    add

--- task-2-messanger-app/messanger-App/src/App.jsx
+++ task-2-messanger-app/messanger-App/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const [name,setUser]=useState("saleem")
+const [chats, setChats]=useState([{name: "ali",message:"hello how are you?"},{name: "saleem",message:"i am good ali."}])
+
+const [mesg ,setMesg]=useState('');
+
+function sendChat(){
+  const c=[...chats];
+  c.push({name,message:mesg});
+  setChats(c)
+  setMesg('')
 }
 
-export default App
+
+  return (
+    
+    <>
+    <h1>User: {name}</h1>
+    <div className="chatContainer ">
+   {chats.map((value)=><div className={`container ${value.name===name ? 'me':''}`}>
+    <p className="chatBox">
+      <strong>{value.name}</strong>
+      <span className={` ${value.name===name ? 'myMessage': ''}`}>{value.message}</span>
+    </p>
+    </div>)}
+    </div>
+    <input onInput={(e)=>setMesg(e.target.value)} type="text" name="text" value={mesg} id="text" placeholder='input your message'/>
+    <button onClick={sendChat} type="submit">Send</button>
+    </>
+    
+  )
+}
